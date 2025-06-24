cbd6168224439ebd768f59b8d4e832554d8378a2
S-design <e.sisley@icloud.com>
2025-06-24 18:32:52 +0100

    sockets n data

--- TCP.py
+++ TCP.py
@@ -2,3 +2,18 @@ import socket
 
 target_host = "www.google.com"
 target_port = 80
+
+#creating a socket object
+client = socket.socket(socket.AF_INET, socket.Sock_STREAM)
+
+#connecting to the client
+client.connect((target_host,target_port))
+
+#sending some data
+client.send(b"GET / HTTP/1.1\r\nHost: google.com\r\n\r\n")
+
+#recieve some data
+response = client.recv(4096)
+
+print(response.decode())
+client.close()
